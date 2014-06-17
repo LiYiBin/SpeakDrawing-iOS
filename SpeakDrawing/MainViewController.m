adf78392df1d054225b6623186141ef8495d83b4
LiYiBin <liyibin.tw@gmail.com>
2014-06-17 23:40:35 +0800

    add core bluetooth

--- SpeakDrawing/MainViewController.m
+++ SpeakDrawing/MainViewController.m
@@ -8,16 +8,26 @@
 
 #import "MainViewController.h"
 
+#import <CoreBluetooth/CoreBluetooth.h>
 #import <OpenEars/LanguageModelGenerator.h>
 #import <OpenEars/AcousticModel.h>
 #import <OpenEars/PocketsphinxController.h>
 #import <OpenEars/OpenEarsEventsObserver.h>
 
-@interface MainViewController () <OpenEarsEventsObserverDelegate>
+#define kInit 0x00
+#define kChangeSingleLedColor 0x01
+
+#define RBL_SERVICE_UUID @"713D0000-503E-4C75-BA94-3148F18D941E"
+#define RBL_CHAR_TX_UUID @"713D0002-503E-4C75-BA94-3148F18D941E"
+#define RBL_CHAR_RX_UUID @"713D0003-503E-4C75-BA94-3148F18D941E"
+#define RBL_BLE_FRAMEWORK_VER 0x0200
+
+@interface MainViewController () <OpenEarsEventsObserverDelegate, CBCentralManagerDelegate, CBPeripheralDelegate>
 
 - (IBAction)touchDownMicrophone:(id)sender;
 - (IBAction)touchUpMicrophone:(id)sender;
 
+// for speech recognition
 @property (strong, nonatomic) LanguageModelGenerator *languageModelGenerator;
 @property (strong, nonatomic) PocketsphinxController *pocketspinxController;
 @property (strong, nonatomic) OpenEarsEventsObserver *openEarsEventsObserver;
@@ -27,6 +37,15 @@
 @property (strong, nonatomic) NSString *dicPath;
 
 - (void)setupSpeechRecognition;
+
+// for bluetooth
+@property (strong, nonatomic) CBCentralManager *centralManager;
+@property (strong, nonatomic) CBPeripheral *activePeripheral;
+
+// for effect
+- (void)sendLedDataByBLE:(NSData *)data;
+- (void)sendInit;
+- (void)sendSingleLedColorByBLE:(int)led red:(int)red green:(int)green blue:(int)blue;
 
 @end
 
@@ -38,6 +57,9 @@
     // setup background
     self.view.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"Background1"]];
 
+    // setup for bluetooth
+    self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:@{CBConnectPeripheralOptionNotifyOnNotificationKey: @YES}];
+    
     [self setupSpeechRecognition];
 }
 
@@ -143,6 +165,181 @@
 }
 - (void) testRecognitionCompleted {
 	NSLog(@"A test file that was submitted for recognition is now complete.");
+}
+
+#pragma mark - Core Bluetooth
+#pragma mark CBCentralManagerDelegate
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    NSLog(@"Central Manager did update state: %@", [central description]);
+    if (central.state != CBCentralManagerStatePoweredOn) {
+        return;
+    }
+    
+    [central scanForPeripheralsWithServices:@[[CBUUID UUIDWithString:RBL_SERVICE_UUID]] options:nil];
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI
+{
+    NSLog(@"Dicovered peripheral: %@", peripheral.identifier);
+    if (self.activePeripheral != peripheral) {
+        self.activePeripheral = peripheral;
+        self.activePeripheral.delegate = self;
+        
+        [central connectPeripheral:peripheral options:@{CBConnectPeripheralOptionNotifyOnNotificationKey: @YES}];
+        [central stopScan];
+    }
+}
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    NSLog(@"Connect peripheral: %@, %@", peripheral.name, [peripheral.identifier UUIDString]);
+    
+    [peripheral discoverServices:nil];
+}
+
+- (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    NSLog(@"Fail to connect peripheral: %@, %@", [peripheral.identifier UUIDString], error);
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"BLE" message:@"Connection failed" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
+}
+
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    NSLog(@"Disconnect peripheral: %@, %@", peripheral.name, [peripheral.identifier UUIDString]);
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"BLE" message:@"Disconnect" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
+    
+    self.activePeripheral = nil;
+    [central scanForPeripheralsWithServices:nil options:nil];
+    [central retrievePeripheralsWithIdentifiers:@[peripheral.identifier]];
+}
+
+#pragma mark CBPeripheralDelegate
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
+{
+    if (error) {
+        NSLog(@"Service discovery was unsuccessful!");
+        return;
+    }
+    
+    NSLog(@"Services of peripheral with UUID : %@ found", [peripheral.identifier UUIDString]);
+    
+    for (CBService *service in peripheral.services) {
+        NSLog(@"Discovered service %@", service);
+        [peripheral discoverCharacteristics:nil forService:service];
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
+{
+    if (error) {
+        NSLog(@"Characteristic discorvery unsuccessful!");
+        return;
+    }
+    
+    for (CBCharacteristic *characteristic in service.characteristics) {
+        NSLog(@"Discovered characteristic %@", characteristic);
+        
+        if ([[service.UUID UUIDString] isEqualToString:RBL_SERVICE_UUID]) {
+            [self sendInit];
+            break;
+        }
+    }
+}
+
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    if (error) {
+        NSLog(@"Characteristic update unsuccessful!");
+        return;
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    if (error) {
+        NSLog(@"Error changing notification state: %@", [error localizedDescription]);
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    if (error) {
+        NSLog(@"Error writing characteristic value: %@", [error localizedDescription]);
+    }
+}
+
+#pragma mark custom core bluetooth methods
+
+- (CBCharacteristic *)findCharacteristicOfLedEffect
+{
+    CBService *service = [self findServiceOfLedEffect];
+    
+    if (!service) {
+        NSLog(@"Find service unsuccessful!");
+        return nil;
+    }
+    
+    for (CBCharacteristic *characteristic in service.characteristics) {
+        if ([[characteristic.UUID UUIDString] isEqualToString:RBL_CHAR_RX_UUID]) {
+            return characteristic;
+        }
+    }
+    
+    return nil;
+}
+
+- (CBService *)findServiceOfLedEffect
+{
+    for (CBService *service in self.activePeripheral.services) {
+        if ([[service.UUID UUIDString] isEqualToString:RBL_SERVICE_UUID]) {
+            return service;
+        }
+    }
+    
+    return nil;
+}
+
+#pragma mark send effect by BLE
+
+- (void)sendInit
+{
+    UInt8 buf[] = {kInit};
+    NSData *data = [NSData dataWithBytes:buf length:1];
+    
+    [self sendLedDataByBLE:data];
+}
+
+- (void)sendSingleLedColorByBLE:(int)led red:(int)red green:(int)green blue:(int)blue
+{
+    UInt8 buf[] = {kChangeSingleLedColor, 0x00, 0x00, 0x00, 0x00};
+    buf[1] = led;
+    buf[2] = red;
+    buf[3] = green;
+    buf[4] = blue;
+    
+    NSData *data = [NSData dataWithBytes:buf length:5];
+    
+    [self sendLedDataByBLE:data];
+}
+
+- (void)sendLedDataByBLE:(NSData *)data
+{
+    CBCharacteristic *characteristic = [self findCharacteristicOfLedEffect];
+    
+    if (!characteristic) {
+        NSLog(@"Find characteristic unsuccessful!");
+        return;
+    }
+
+    [self.activePeripheral writeValue:data forCharacteristic:characteristic type:CBCharacteristicWriteWithoutResponse];
 }
 
 @end
